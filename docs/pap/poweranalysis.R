5214242348280c53f5221d65a1ac26a6bdcb903f
MarikenvdVelden <>
2021-04-13 18:45:45 +0200

    pap info

--- docs/pap/poweranalysis.R
+++ docs/pap/poweranalysis.R
@@ -20,31 +20,14 @@ designs <- redesign(design=two_arm_design, N=seq(500,2500,250), treatment_effect
 alpha <- .05
 my_diagnosands <- declare_diagnosands(power.onetailed=mean(p.value<alpha), keep_defaults=TRUE)
 diagnosis1 <- diagnose_designs(designs,diagnosands=my_diagnosands)
-diagnosis <- diagnosis1$diagnosands_df[,c(2,3,7)]
+diagnosis <- diagnosis1$diagnosands_df
+diagnosis1  <- diagnosis %>% 
+  select(N, treatment_effect, power.onetailed) %>%
+  mutate(id = "Power-Analysis for H1 and H2")
 
-ggplot(diagnosis, aes(x=N, y=power.onetailed, group=factor(treatment_effect), colour=factor(treatment_effect))) +
-  geom_line() +
-  geom_hline(yintercept=0.95, linetype="dashed") +
-  geom_hline(yintercept=0.8, linetype="dashed", color = "grey") +
-  geom_vline(xintercept = 2000,linetype="dotdash", color = "darkgrey") +
-  ylim(c(0,1)) +
-  labs(x = "Number of Respondents", y = "Power: One-Tailed",
-       subtitle = "Power-Analysis for H1 and H2") +
-  scale_y_continuous(labels=percent) +
-  theme_minimal() +
-  scale_color_viridis_d() +
-  theme(plot.title = element_text(hjust = 0.5),
-        plot.subtitle = element_text(hjust = 0.5),
-        legend.position="bottom",
-        legend.title = element_blank()) +
-  guides(color=guide_legend(nrow=1,byrow=TRUE))
-
-  
 #H3
 N <- 100
-assignment_prob <- 0.125 
-# only 1 in 8 conditions is have women + migration background + compromise condition. 
-# So there is a 1/8 you are exposed to the full women + migration background compromise condition
+assignment_prob <- 0.125 # only 1 in 8 conditions is have women + migration background + compromise condition. 
 treatment_effect <- 0.1
 
 population <- declare_population(N = N, u = rnorm(N))
@@ -60,32 +43,35 @@ designs <- redesign(design=two_arm_design, N=seq(500,2500,250), treatment_effect
 alpha <- .05
 my_diagnosands <- declare_diagnosands(power.onetailed=mean(p.value<alpha), keep_defaults=TRUE)
 diagnosis2 <- diagnose_designs(designs,diagnosands=my_diagnosands)
-diagnosis <- diagnosis2$diagnosands_df[,c(2,3,7)]
-  
-ggplot(diagnosis, aes(x=N, y=power.onetailed, group=factor(treatment_effect), colour=factor(treatment_effect))) +
+diagnosis <- diagnosis2$diagnosands_df
+diagnosis2  <- diagnosis %>% 
+  select(N, treatment_effect, power.onetailed) %>%
+  mutate(id = "Power-Analysis for H3")
+
+diagnosis1 %>% add_case(diagnosis2) %>%
+  ggplot(aes(x=N, y=power.onetailed, group=factor(treatment_effect), colour=factor(treatment_effect))) +
   geom_line() +
   geom_hline(yintercept=0.95, linetype="dashed") +
   geom_hline(yintercept=0.8, linetype="dashed", color = "grey") +
   geom_vline(xintercept = 2000,linetype="dotdash", color = "darkgrey") +
   ylim(c(0,1)) +
   labs(x = "Number of Respondents", y = "Power: One-Tailed",
-       subtitle = "Power-Analysis for H3") +
+       subtitle = "Various Levels of Treatment Effect") +
   scale_y_continuous(labels=percent) +
   theme_minimal() +
+  facet_grid(.~id) +
   scale_color_viridis_d() +
   theme(plot.title = element_text(hjust = 0.5),
         plot.subtitle = element_text(hjust = 0.5),
         legend.position="bottom",
         legend.title = element_blank()) +
   guides(color=guide_legend(nrow=1,byrow=TRUE))
-
+ggsave(here("docs/pap/power_analysis.eps"), width=8, height=5, dpi=900)
 
 #Pooling data for .1 effect two-tailed
 #H1 & H2
 N <- 100
-assignment_prob <- 0.25 
-# only 2 in 8 conditions is have women/migration background + compromise condition. 
-# So there is a 1/4 you are exposed to the full women/migration background compromise condition
+assignment_prob <- 0.25 # only 2 in 8 conditions is have women/migration background + compromise condition. 
 treatment_effect <- 0.1
 
 population <- declare_population(N = N, u = rnorm(N))
@@ -101,30 +87,14 @@ designs <- redesign(design=two_arm_design, N=seq(2000,8000,500), treatment_effec
 alpha <- .05
 my_diagnosands <- declare_diagnosands(power.twotailed=mean(p.value<alpha), keep_defaults=TRUE)
 diagnosis3 <- diagnose_designs(designs,diagnosands=my_diagnosands)
-diagnosis <- diagnosis3$diagnosands_df[,c(2,3,7)]
-
-ggplot(diagnosis, aes(x=N, y=power.twotailed, group=factor(treatment_effect), colour=factor(treatment_effect))) +
-  geom_line() +
-  geom_hline(yintercept=0.95, linetype="dashed") +
-  geom_hline(yintercept=0.8, linetype="dashed", color = "grey") +
-  #geom_vline(xintercept = 2000,linetype="dotdash", color = "darkgrey") +
-  ylim(c(0,1)) +
-  labs(x = "Number of Respondents", y = "Power: Two-Tailed",
-       subtitle = "Power-Analysis for H1 and H2") +
-  scale_y_continuous(labels=percent) +
-  theme_minimal() +
-  scale_color_viridis_d() +
-  theme(plot.title = element_text(hjust = 0.5),
-        plot.subtitle = element_text(hjust = 0.5),
-        legend.position="none",
-        legend.title = element_blank()) +
-  guides(color=guide_legend(nrow=1,byrow=TRUE))
+diagnosis <- diagnosis3$diagnosands_df
+diagnosis3  <- diagnosis %>% 
+  select(N, treatment_effect, power.twotailed) %>%
+  mutate(id = "Power-Analysis for H1 and H2")
 
 #H3
 N <- 100
-assignment_prob <- 0.125 
-# only 2 in 8 conditions is have women/migration background + compromise condition. 
-# So there is a 1/4 you are exposed to the full women/migration background compromise condition
+assignment_prob <- 0.125 # only 2 in 8 conditions is have women/migration background + compromise condition. 
 treatment_effect <- 0.1
 
 population <- declare_population(N = N, u = rnorm(N))
@@ -140,16 +110,20 @@ designs <- redesign(design=two_arm_design, N=seq(2000,8000,500), treatment_effec
 alpha <- .05
 my_diagnosands <- declare_diagnosands(power.twotailed=mean(p.value<alpha), keep_defaults=TRUE)
 diagnosis4 <- diagnose_designs(designs,diagnosands=my_diagnosands)
-diagnosis <- diagnosis4$diagnosands_df[,c(2,3,7)]
+diagnosis <- diagnosis4$diagnosands_df
+diagnosis4  <- diagnosis %>% 
+  select(N, treatment_effect, power.twotailed) %>%
+  mutate(id = "Power-Analysis for H3")
 
-ggplot(diagnosis, aes(x=N, y=power.twotailed, group=factor(treatment_effect), colour=factor(treatment_effect))) +
+diagnosis3 %>% add_case(diagnosis4) %>%
+  ggplot(aes(x=N, y=power.twotailed, group=factor(treatment_effect), colour=factor(treatment_effect))) +
   geom_line() +
   geom_hline(yintercept=0.95, linetype="dashed") +
   geom_hline(yintercept=0.8, linetype="dashed", color = "grey") +
-  #geom_vline(xintercept = 2000,linetype="dotdash", color = "darkgrey") +
+  facet_grid(.~id) +
   ylim(c(0,1)) +
   labs(x = "Number of Respondents", y = "Power: Two-Tailed",
-       subtitle = "Power-Analysis for H3") +
+       subtitle = "Treatment Effect = 0.1") +
   scale_y_continuous(labels=percent) +
   theme_minimal() +
   scale_color_viridis_d() +
@@ -158,3 +132,4 @@ ggplot(diagnosis, aes(x=N, y=power.twotailed, group=factor(treatment_effect), co
         legend.position="none",
         legend.title = element_blank()) +
   guides(color=guide_legend(nrow=1,byrow=TRUE))
+ggsave(here("docs/pap/power_analysis-pooled.eps"), width=8, height=5, dpi=900)
--- src/data-processing/prep_data.md
+++ src/data-processing/prep_data.md
@@ -20,33 +20,7 @@ rm(list=ls())
 renv::snapshot()
 ```
 
-    ## The following package(s) will be updated in the lockfile:
-    ## 
-    ## # CRAN ===============================
-    ## - Rcpp        [* -> 1.0.5]
-    ## - digest      [* -> 0.6.27]
-    ## - evaluate    [* -> 0.14]
-    ## - glue        [* -> 1.4.2]
-    ## - here        [* -> 1.0.1]
-    ## - highr       [* -> 0.8]
-    ## - htmltools   [* -> 0.4.0]
-    ## - jsonlite    [* -> 1.7.1]
-    ## - knitr       [* -> 1.30]
-    ## - magrittr    [* -> 1.5]
-    ## - markdown    [* -> 1.1]
-    ## - mime        [* -> 0.9]
-    ## - printr      [* -> 0.1]
-    ## - renv        [* -> 0.12.2]
-    ## - rlang       [* -> 0.4.8]
-    ## - rmarkdown   [* -> 2.5]
-    ## - rprojroot   [* -> 2.0.2]
-    ## - stringi     [* -> 1.5.3]
-    ## - stringr     [* -> 1.4.0]
-    ## - tinytex     [* -> 0.20]
-    ## - xfun        [* -> 0.18]
-    ## - yaml        [* -> 2.2.1]
-    ## 
-    ## * Lockfile written to '~/Dropbox/Papers/compromise-punish/src/data-processing/renv.lock'.
+    ## * The lockfile is already up to date.
 
 ``` r
 source(here::here("src/lib/functions.R"))
@@ -78,33 +52,7 @@ tr <- fetch_survey(surveyID = "SV_7aEiPm6fLzSon4O",
                   force_request = TRUE, verbose = TRUE,
                   label = FALSE, convert = FALSE) %>%
   filter(Consent == 1) %>%
-  select(matches("C-[A-Z]\\d"), id = ResponseId) %>%
-  pivot_longer(cols = `C-D1`:`C-D8`,
-               names_to = "Def") %>%
-  drop_na(value) %>%
-  select(-value) %>%
-  pivot_longer(cols = `C-E1`:`C-E8`,
-               names_to = "Edu") %>%
-  drop_na(value) %>%
-  select(-value) %>%
-  pivot_longer(cols = `C-I1`:`C-I8`,
-               names_to = "Imm") %>%
-  drop_na(value) %>%
-  select(-value) %>%
-  pivot_longer(cols = `C-C1`:`C-C8`,
-               names_to = "COVID") %>%
-  drop_na(value) %>%
-  select(-value) %>%
-  mutate(across(-id, ~str_sub(., -1))) %>%
-  mutate(across(-id, ~recode(.,
-                       `1` = "Rachid Amezian + Compromis",
-                       `2` = "Rachid Amezian + Geen compromis",
-                       `3` = "Karel van der Kleijn + Compromis",
-                       `4` = "Karel van der Kleijn + Geen compromis",
-                       `5` = "Rachida Amezian + Compromis",
-                       `6` = "Rachida Amezian + Geen compromis",
-                       `7` = "Karin van der Kleijn + Compromis",
-                       `8` = "Karin van der Kleijn + Geen compromis")))
+  select(matches("name-[A-Z]"), compromis, id = ResponseId)
 ```
 
     ##   |                                                                              |                                                                      |   0%  |                                                                              |======================================================================| 100%
