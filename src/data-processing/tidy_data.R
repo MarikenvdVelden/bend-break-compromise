044f140b8dd6bcb3362cd2fbbd867a5b5a1740fc
MarikenvdVelden <>
2021-05-27 15:25:05 +0200

    change mistake in coding compromise in treatment

--- src/data-processing/tidy_data.R
+++ src/data-processing/tidy_data.R
@@ -14,11 +14,118 @@ pret <- read_sav(here("data/raw-private/experiment.sav")) %>%
                         `12` = 2, `11` = 1))) %>%
   drop_na()
 
-tr <- read_sav(here("data/raw-private/experiment.sav")) %>%
+tmp <- read_sav(here("data/raw-private/experiment.sav")) %>%
   remove_all_labels() %>%
   filter(Consent == 1, Attention2_3 ==1, Attention2_5==1) %>%
-  select(matches("name_[A-Z]"), compromis, id = ResponseId) %>%
-  drop_na(name_I, name_E)
+  select(matches("FL_"),id = ResponseId)
+
+tr1 <- read_sav(here("data/raw-private/experiment.sav")) %>%
+  remove_all_labels() %>%
+  filter(Consent == 1, Attention2_3 == 1, Attention2_5 == 1) %>%
+  select(matches("FL_"),id = ResponseId) %>%
+  mutate(FL_32_DO_FL_104 = recode(FL_32_DO_FL_104, `1` = "Rachid Amezian", .default = "NO"),
+         FL_32_DO_FL_106 = recode(FL_32_DO_FL_106, `1` = "Rachid Amezian", .default = "NO"),
+         FL_32_DO_FL_108 = recode(FL_32_DO_FL_108, `1` = "Karel van der Kleijn", .default = "NO"),
+         FL_32_DO_FL_110 = recode(FL_32_DO_FL_110, `1` = "Karel van der Kleijn", .default = "NO"),
+         FL_32_DO_FL_112 = recode(FL_32_DO_FL_112, `1` = "Rachida Amezian", .default = "NO"),
+         FL_32_DO_FL_114 = recode(FL_32_DO_FL_114, `1` = "Rachida Amezian", .default = "NO"),
+         FL_32_DO_FL_116 = recode(FL_32_DO_FL_116, `1` = "Karin van der Kleijn", .default = "NO"),
+         FL_32_DO_FL_118 = recode(FL_32_DO_FL_118, `1` = "Karin van der Kleijn", .default = "NO"),
+         FL_78_DO_FL_123 = recode(FL_78_DO_FL_123, `1` = "Rachid Amezian", .default = "NO"),
+         FL_78_DO_FL_125 = recode(FL_78_DO_FL_125, `1` = "Rachid Amezian", .default = "NO"),
+         FL_78_DO_FL_129 = recode(FL_78_DO_FL_129, `1` = "Karel van der Kleijn", .default = "NO"),
+         FL_78_DO_FL_132 = recode(FL_78_DO_FL_132, `1` = "Karel van der Kleijn", .default = "NO"),
+         FL_78_DO_FL_134 = recode(FL_78_DO_FL_134, `1` = "Rachida Amezian", .default = "NO"),
+         FL_78_DO_FL_137 = recode(FL_78_DO_FL_137, `1` = "Rachida Amezian", .default = "NO"),
+         FL_78_DO_FL_139 = recode(FL_78_DO_FL_139, `1` = "Karin van der Kleijn", .default = "NO"),
+         FL_78_DO_FL_142 = recode(FL_78_DO_FL_142, `1` = "Karin van der Kleijn", .default = "NO"),
+         FL_84_DO_FL_144 = recode(FL_84_DO_FL_144, `1` = "Rachid Amezian", .default = "NO"),
+         FL_84_DO_FL_145 = recode(FL_84_DO_FL_145, `1` = "Rachid Amezian", .default = "NO"),
+         FL_84_DO_FL_148 = recode(FL_84_DO_FL_148, `1` = "Karel van der Kleijn", .default = "NO"),
+         FL_84_DO_FL_151 = recode(FL_84_DO_FL_151, `1` = "Karel van der Kleijn", .default = "NO"),
+         FL_84_DO_FL_153 = recode(FL_84_DO_FL_153, `1` = "Rachida Amezian", .default = "NO"),
+         FL_84_DO_FL_157 = recode(FL_84_DO_FL_157, `1` = "Rachida Amezian", .default = "NO"),
+         FL_84_DO_FL_159 = recode(FL_84_DO_FL_159, `1` = "Karin van der Kleijn", .default = "NO"),
+         FL_84_DO_FL_161 = recode(FL_84_DO_FL_161, `1` = "Karin van der Kleijn", .default = "NO"),
+         FL_101_DO_FL_163 = recode(FL_101_DO_FL_163, `1` = "Rachid Amezian", .default = "NO"),
+         FL_101_DO_FL_165 = recode(FL_101_DO_FL_165, `1` = "Rachid Amezian", .default = "NO"),
+         FL_101_DO_FL_167 = recode(FL_101_DO_FL_167, `1` = "Karel van der Kleijn", .default = "NO"),
+         FL_101_DO_FL_169 = recode(FL_101_DO_FL_169, `1` = "Karel van der Kleijn", .default = "NO"),
+         FL_101_DO_FL_171 = recode(FL_101_DO_FL_171, `1` = "Rachida Amezian", .default = "NO"),
+         FL_101_DO_FL_173 = recode(FL_101_DO_FL_173, `1` = "Rachida Amezian", .default = "NO"),
+         FL_101_DO_FL_175 = recode(FL_101_DO_FL_175, `1` = "Karin van der Kleijn", .default = "NO"),
+         FL_101_DO_FL_177 = recode(FL_101_DO_FL_177, `1` = "Karin van der Kleijn", .default = "NO")) %>%
+  pivot_longer(cols = c(FL_32_DO_FL_104:FL_32_DO_FL_118),
+               values_to = "name_D") %>%
+  drop_na("name_D") %>%
+  select(-name) %>%
+  pivot_longer(cols = c(FL_78_DO_FL_123:FL_78_DO_FL_142),
+               values_to = "name_I") %>%
+  drop_na("name_I") %>%
+  select(-name) %>%
+  pivot_longer(cols = c(FL_84_DO_FL_144:FL_84_DO_FL_161),
+               values_to = "name_E") %>%
+  drop_na("name_E") %>%
+  select(-name) %>%
+  pivot_longer(cols = c(FL_101_DO_FL_163:FL_101_DO_FL_177),
+               values_to = "name_C") %>%
+  drop_na("name_C") %>%
+  select(-name)
+
+tr2 <- read_sav(here("data/raw-private/experiment.sav")) %>%
+  remove_all_labels() %>%
+  filter(Consent == 1, Attention2_3 == 1, Attention2_5 == 1) %>%
+  select(matches("FL_"),id = ResponseId) %>%
+  mutate(FL_32_DO_FL_104 = recode(FL_32_DO_FL_104, `1` = "een compromis", .default = "NO"),
+         FL_32_DO_FL_106 = recode(FL_32_DO_FL_106, `1` = "geen compromis", .default = "NO"),
+         FL_32_DO_FL_108 = recode(FL_32_DO_FL_108, `1` = "een compromis", .default = "NO"),
+         FL_32_DO_FL_110 = recode(FL_32_DO_FL_110, `1` = "geen compromis", .default = "NO"),
+         FL_32_DO_FL_112 = recode(FL_32_DO_FL_112, `1` = "een compromis", .default = "NO"),
+         FL_32_DO_FL_114 = recode(FL_32_DO_FL_114, `1` = "geen compromis", .default = "NO"),
+         FL_32_DO_FL_116 = recode(FL_32_DO_FL_116, `1` = "een compromis", .default = "NO"),
+         FL_32_DO_FL_118 = recode(FL_32_DO_FL_118, `1` = "geen compromis", .default = "NO"),
+         FL_78_DO_FL_123 = recode(FL_78_DO_FL_123, `1` = "een compromis", .default = "NO"),
+         FL_78_DO_FL_125 = recode(FL_78_DO_FL_125, `1` = "geen compromis", .default = "NO"),
+         FL_78_DO_FL_129 = recode(FL_78_DO_FL_129, `1` = "een compromis", .default = "NO"),
+         FL_78_DO_FL_132 = recode(FL_78_DO_FL_132, `1` = "geen compromis", .default = "NO"),
+         FL_78_DO_FL_134 = recode(FL_78_DO_FL_134, `1` = "een compromis", .default = "NO"),
+         FL_78_DO_FL_137 = recode(FL_78_DO_FL_137, `1` = "geen compromis", .default = "NO"),
+         FL_78_DO_FL_139 = recode(FL_78_DO_FL_139, `1` = "een compromis", .default = "NO"),
+         FL_78_DO_FL_142 = recode(FL_78_DO_FL_142, `1` = "geen compromis", .default = "NO"),
+         FL_84_DO_FL_144 = recode(FL_84_DO_FL_144, `1` = "een compromis", .default = "NO"),
+         FL_84_DO_FL_145 = recode(FL_84_DO_FL_145, `1` = "geen compromis", .default = "NO"),
+         FL_84_DO_FL_148 = recode(FL_84_DO_FL_148, `1` = "een compromis", .default = "NO"),
+         FL_84_DO_FL_151 = recode(FL_84_DO_FL_151, `1` = "geen compromis", .default = "NO"),
+         FL_84_DO_FL_153 = recode(FL_84_DO_FL_153, `1` = "een compromis", .default = "NO"),
+         FL_84_DO_FL_157 = recode(FL_84_DO_FL_157, `1` = "geen compromis", .default = "NO"),
+         FL_84_DO_FL_159 = recode(FL_84_DO_FL_159, `1` =  "een compromis", .default = "NO"),
+         FL_84_DO_FL_161 = recode(FL_84_DO_FL_161, `1` = "geen compromis", .default = "NO"),
+         FL_101_DO_FL_163 = recode(FL_101_DO_FL_163, `1` = "een compromis", .default = "NO"),
+         FL_101_DO_FL_165 = recode(FL_101_DO_FL_165, `1` = "geen compromis", .default = "NO"),
+         FL_101_DO_FL_167 = recode(FL_101_DO_FL_167, `1` = "een compromis", .default = "NO"),
+         FL_101_DO_FL_169 = recode(FL_101_DO_FL_169, `1` = "geen compromis", .default = "NO"),
+         FL_101_DO_FL_171 = recode(FL_101_DO_FL_171, `1` = "een compromis", .default = "NO"),
+         FL_101_DO_FL_173 = recode(FL_101_DO_FL_173, `1` = "geen compromis", .default = "NO"),
+         FL_101_DO_FL_175 = recode(FL_101_DO_FL_175, `1` = "een compromis", .default = "NO"),
+         FL_101_DO_FL_177 = recode(FL_101_DO_FL_177, `1` = "geen compromis", .default = "NO")) %>%
+  pivot_longer(cols = c(FL_32_DO_FL_104:FL_32_DO_FL_118),
+               values_to = "compromise_D") %>%
+  drop_na("compromise_D") %>%
+  select(-name) %>%
+  pivot_longer(cols = c(FL_78_DO_FL_123:FL_78_DO_FL_142),
+               values_to = "compromise_I") %>%
+  drop_na("compromise_I") %>%
+  select(-name) %>%
+  pivot_longer(cols = c(FL_84_DO_FL_144:FL_84_DO_FL_161),
+               values_to = "compromise_E") %>%
+  drop_na("compromise_E") %>%
+  select(-name) %>%
+  pivot_longer(cols = c(FL_101_DO_FL_163:FL_101_DO_FL_177),
+               values_to = "compromise_C") %>%
+  drop_na("compromise_C") %>%
+  select(-name)
+
+tr <- inner_join(tr1, tr2, by = "id")
 
 pt <- read_sav(here("data/raw-private/experiment.sav")) %>%
   remove_all_labels() %>%
@@ -63,4 +170,4 @@ bg <- read_sav(here("data/raw-private/experiment.sav")) %>%
 d <- left_join(pret, bg, by = "id")
 d <- left_join(d, tr, by = "id")
 d <- left_join(d, pt, by = "id") 
-rm(bg, pret, tr, pt)
+rm(bg, pret, tr1, tr2, tr, pt)
